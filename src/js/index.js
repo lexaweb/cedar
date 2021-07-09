9ae4898ee4a432ff563b0f64d2e96caddb94b107
Alexey Stadnik <aleksei.stadnik@dterra.eu>
2021-07-09 08:44:27 +0300

    add prew

--- src/js/index.js
+++ src/js/index.js
@@ -14,6 +14,7 @@ $(function () {
   sliderInit()
   select()
   slideractiveInit()
+  count()
 })
 
 function sidebarInit() {
@@ -58,5 +59,22 @@ function slideractiveInit() {
     $('.filter-decor-slider-item')
       .not(this)
       .removeClass('active');
+  });
+}
+
+function count() {
+  $('.minus').on('click', function () {
+    var $input = $(this).parent().find('input');
+    var count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+    $input.change();
+    return false;
+  });
+  $('.plus').on('click', function () {
+    var $input = $(this).parent().find('input');
+    $input.val(parseInt($input.val()) + 1);
+    $input.change();
+    return false;
   });
 }
